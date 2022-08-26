7fe66617657ffc4c2efb0b88ff59dc2827e93772
dantasg <genineto10@gmail.com>
2022-08-26 11:06:33 -0300

    'Initial'

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+export const PostCard = ({title, cover, body, id}) => {
+
+    // const {post} = props;
+
+    return (
+        <div className='post'>
+            <img src={cover} alt={title}></img>
+            <div className="post-content">
+                <h1> {title} {id}</h1>
+                <p>{body}</p>
+            </div>
+        </div>
+    );
+}
